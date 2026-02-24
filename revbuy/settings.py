4cf377d0c4155628a267c76257a0beaafb2daa49
Himanshu <himanshu19704@gmail.com>
2026-02-24 14:27:13 +0530

    fixed all hosts

--- revbuy/settings.py
+++ revbuy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-f74-trtueoydq9iqs)8rfn#-+ag)xz!a(kx@!rwx*blcgtivh!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
